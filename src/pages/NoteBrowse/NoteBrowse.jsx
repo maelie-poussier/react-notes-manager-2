c600bdbdba75b3abbe9914c63b9ce01eba9829ea
maelie-poussier <maelie.poussier@icloud.com>
2024-07-31 10:34:54 +0200

    ajout ed plusieurs note dans mon fichier json

--- src/pages/NoteBrowse/NoteBrowse.jsx
+++ src/pages/NoteBrowse/NoteBrowse.jsx
@@ -1,7 +1,6 @@
-import s from "./style.module.css"
+import { SearchBar } from "../../components/SearchBar/SearchBar";
 import { TextCard } from "../../components/TextCard/TextCard";
-// import { NoteList } from "../../containers/NoteList/NoteList"
-import { SearchBar } from "../../components/SearchBar/SearchBar"
+import { NoteList } from "../../containers/NoteList/NoteList";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
@@ -9,32 +8,37 @@ import { Link } from "react-router-dom";
 export function NoteBrowse(props) {
   const [searchText, setSearchText] = useState("");
   const noteList = useSelector((store) => store.NOTE.noteList);
-  const filteredList = noteList.filter(note => {
-    const containsTitle = note.title.toUpperCase().includes(searchText.trim().toUpperCase());
+  const filteredList = noteList.filter((note) => {
+    const containsTitle = note.title
+      .toUpperCase()
+      .includes(searchText.trim().toUpperCase());
 
-    const containsContent = note.content.toUpperCase().includes(searchText.trim().toUpperCase());
+    const containsContent = note.content
+      .toUpperCase()
+      .includes(searchText.trim().toUpperCase());
+
     return containsTitle || containsContent;
-  })
-
-  return <>
-    <div className="row justify-content-center mb-5" >
-      <div className="col-sm-12 col-md-5">
-        <SearchBar
-          placeholder="Search your notes ..."
-          onTextChange={setSearchText}
-        />
+  });
+  return (
+    <>
+      <div className="row justify-content-center mb-5">
+        <div className="col-sm-12 col-md-4">
+          <SearchBar
+            placeholder="Search your notes..."
+            onTextChange={setSearchText}
+          />
+        </div>
       </div>
-    </div>
 
-    {
-      filteredList?.length === 0 && (
+      {noteList?.length === 0 && (
         <div className="d-flex justify-content-center">
           <span>
-            Vous n'avez pas de note, voulez vous en
+            Vous n'avez pas de note, voulez vous en{" "}
             <Link to="/note/new"> créer une </Link>
           </span>
         </div>
-      )
-    }
-  </>
+      )}
+      <NoteList noteList={filteredList} />
+    </>
+  );
 }
